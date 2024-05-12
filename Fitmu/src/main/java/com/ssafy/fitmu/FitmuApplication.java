ac13e31e3548d44f469bdaf47090cf15249dc094
Yujachaa <114348883+yujachaa@users.noreply.github.com>
2024-05-12 14:45:12 +0900

    spring boot project create

---
+++ Fitmu/src/main/java/com/ssafy/fitmu/FitmuApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.fitmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmuApplication.class, args);
+	}
+
+}
