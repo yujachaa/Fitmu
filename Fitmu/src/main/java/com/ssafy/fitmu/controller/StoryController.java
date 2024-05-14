f3fd4b5c2a589269dcb579348ec44ee8a74c7019
전성모 <jeonsm22@daum.net>
2024-05-14 22:28:55 +0900

    story

--- Fitmu/src/main/java/com/ssafy/fitmu/controller/StoryController.java
+++ Fitmu/src/main/java/com/ssafy/fitmu/controller/StoryController.java
@@ -27,131 +27,165 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "게시글 컨트롤러")
 public class StoryController {
 	private StoryService storyService;
-	
+
 	public StoryController(StoryService storyService) {
 		this.storyService = storyService;
 	}
-	
+
+	// 게시글 1개 조회
 	@GetMapping("/{storyId}")
-	public ResponseEntity<?> storyDetail(@PathVariable("storyId") int id){
+	public ResponseEntity<?> storyDetail(@PathVariable("storyId") int id) {
 		Story story = storyService.selectOne(id);
-		
-		if(story == null) {
+
+		if (story == null) {
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
+		} else {
 			return new ResponseEntity<Story>(story, HttpStatus.OK);
 		}
 	}
-	
+
+	// 게시글 등록
 	@PostMapping("/regist")
-	public ResponseEntity<?> storyRegist(@RequestBody Story story){
+	public ResponseEntity<?> storyRegist(@RequestBody Story story) {
 		int result = storyService.insertStory(story);
-		
-		if(result == 0) {
+
+		if (result == 0) {
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
+		} else {
 			return new ResponseEntity<Void>(HttpStatus.OK);
 		}
 	}
-	
+
+	// 게시글 수정
 	@PutMapping("/{storyId}")
-	public ResponseEntity<?> storyUpdate(@PathVariable("storyId") int id, @RequestBody Story story){
+	public ResponseEntity<?> storyUpdate(@PathVariable("storyId") int id, @RequestBody Story story) {
 		story.setStoryId(id);
-		
+
 		int result = storyService.updateStory(story);
-		
-		if(result == 0) {
+
+		if (result == 0) {
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
+		} else {
 			return new ResponseEntity<Void>(HttpStatus.OK);
 		}
 	}
-	
+
+	// 게시글 삭제
 	@DeleteMapping("/{storyId}")
-	public ResponseEntity<?> storyDelete(@PathVariable("storyId") int id){
+	public ResponseEntity<?> storyDelete(@PathVariable("storyId") int id) {
 		int result = storyService.deleteStory(id);
-		
-		if(result == 0) {
+
+		if (result == 0) {
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
+		} else {
 			return new ResponseEntity<Void>(HttpStatus.OK);
 		}
 	}
-	
+
+	// 게시글 전체 조회
 	@GetMapping("/stroy")
-	public ResponseEntity<?> storyAll(){
+	public ResponseEntity<?> storyAll() {
 		List<Story> storyList = storyService.selectAll();
-		
-		if(storyList == null || storyList.size() == 0) {
+
+		if (storyList == null || storyList.size() == 0) {
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
+		} else {
 			return new ResponseEntity<List<Story>>(storyList, HttpStatus.OK);
 		}
 	}
-	
+
+	// 게시글 검색
 	@GetMapping("/search")
-	public ResponseEntity<?> search(@ModelAttribute SearchCondition condition){
+	public ResponseEntity<?> search(@ModelAttribute SearchCondition condition) {
 		List<Story> storyList = storyService.searchStory(condition);
-		
-		if(storyList == null || storyList.size() == 0) {
+
+		if (storyList == null || storyList.size() == 0) {
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
+		} else {
 			return new ResponseEntity<List<Story>>(storyList, HttpStatus.OK);
 		}
 	}
-	
+
+	// 해당 유저가 쓴 게시글
 	@GetMapping("/user/{userId}")
-	public ResponseEntity<?> searchByUserId(@PathVariable("userId") int id){
+	public ResponseEntity<?> searchByUserId(@PathVariable("userId") int id) {
 		List<Story> storyList = storyService.selectByUserId(id);
-		
-		if(storyList == null || storyList.size() == 0) {
+
+		if (storyList == null || storyList.size() == 0) {
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
+		} else {
+			return new ResponseEntity<List<Story>>(storyList, HttpStatus.OK);
+		}
+	}
+
+	// 게시글 좋아요 증가
+	@PutMapping("/like/{storyId}")
+	public ResponseEntity<?> likeStory(@PathVariable("storyId") int id) {
+		int result = storyService.increaseLikeCnt(id);
+
+		if (result == 0) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+	}
+
+	// 게시글 좋아요 감소
+	@PutMapping("/dislike/{storyId}")
+	public ResponseEntity<?> disLikeStory(@PathVariable("storyId") int id) {
+		int result = storyService.decreaseLikeCnt(id);
+
+		if (result == 0) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+	}
+
+	// 게시글 스크랩북 등록
+	@PostMapping("/story/{storyId}/user/{userId}")
+	public ResponseEntity<?> registScrapBook(@PathVariable("storyId") int storyId, @PathVariable("userId") int userId) {
+		int result = storyService.insertScrapBook(userId, storyId);
+
+		if (result == 0) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+	}
+
+	// 게시글 스크랩북 삭제
+	@DeleteMapping("/story/{storyId}/user/{userId}")
+	public ResponseEntity<?> removeScrapBook(@PathVariable("storyId") int storyId, @PathVariable("userId") int userId) {
+		int result = storyService.deleteScrapBook(userId, storyId);
+
+		if (result == 0) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+	}
+
+	@GetMapping("/story/popular")
+	public ResponseEntity<?> popularStory() {
+		List<Story> storyList = storyService.popularStory();
+
+		if (storyList == null || storyList.size() == 0) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		} else {
 			return new ResponseEntity<List<Story>>(storyList, HttpStatus.OK);
 		}
 	}
 	
-	@PutMapping("/like/{storyId}")
-	public ResponseEntity<?> likeStory(@PathVariable("storyId") int id){
-		int result = storyService.increaseLikeCnt(id);
-		
-		if(result == 0) {
+	@GetMapping("/story/like")
+	public ResponseEntity<?> likeStory() {
+		List<Story> storyList = storyService.likeStory();
+
+		if (storyList == null || storyList.size() == 0) {
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
-			return new ResponseEntity<Void>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<List<Story>>(storyList, HttpStatus.OK);
 		}
 	}
-	
-	@PutMapping("/dislike/{storyId}")
-	public ResponseEntity<?> disLikeStory(@PathVariable("storyId") int id){
-		int result = storyService.decreaseLikeCnt(id);
-		
-		if(result == 0) {
-			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
-			return new ResponseEntity<Void>(HttpStatus.OK);
-		}
-	}
-	
-	@PostMapping("/story/{storyId}/user/{userId}")
-	public ResponseEntity<?> registScrapBook(@PathVariable("storyId") int storyId, @PathVariable("userId") int userId){
-		int result = storyService.insertScrapBook(userId, storyId);
-		
-		if(result == 0) {
-			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
-			return new ResponseEntity<Void>(HttpStatus.OK);
-		}
-	}
-	
-	@DeleteMapping("/story/{storyId}/user/{userId}")
-	public ResponseEntity<?> removeScrapBook(@PathVariable("storyId") int storyId, @PathVariable("userId") int userId){
-		int result = storyService.deleteScrapBook(userId, storyId);
-		
-		if(result == 0) {
-			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-		}else {
-			return new ResponseEntity<Void>(HttpStatus.OK);
-		}
-	}
+
 }
